c40028dd03fd1d820514bc6f4fb8e4a80a08f8df
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2018-08-22 22:33:54 -0700

    PF-179 Improve the time-series frequency detection logic
    Look at the last 100 points, and determine the most frequent period between points. Use that period.

--- SosExporter/Exporter.cs
+++ SosExporter/Exporter.cs
@@ -642,25 +642,49 @@ namespace SosExporter
             if (recentPoints.Count < 2)
                 return ComputationPeriod.Unknown;
 
-            var duration = recentPoints.Last().Timestamp.DateTimeOffset -
-                           recentPoints.First().Timestamp.DateTimeOffset;
+            // Create some bins with zero counts
+            var periodFrequency = new Dictionary<ComputationPeriod,int>();
 
-            var averageDuration = TimeSpan.FromTicks(duration.Ticks / recentPoints.Count);
+            for (var i = 0; i < recentPoints.Count - 1; ++i)
+            {
+                var timeSpan = recentPoints[i + 1].Timestamp.DateTimeOffset
+                    .Subtract(recentPoints[i].Timestamp.DateTimeOffset);
 
-            if (averageDuration < CommonPeriods.First().TimeSpan)
+                var period = FindClosestPeriod(timeSpan);
+
+                if (!periodFrequency.ContainsKey(period))
+                {
+                    periodFrequency.Add(period, 0);
+                }
+
+                periodFrequency[period] += 1;
+            }
+
+            // Find the most frequent bin
+            var mostCommonPeriod = periodFrequency
+                .Select(kvp => (kvp.Key, kvp.Value))
+                .OrderByDescending(kvp => kvp.Item2)
+                .First().Item1;
+
+            return mostCommonPeriod;
+        }
+
+        private static ComputationPeriod FindClosestPeriod(TimeSpan timeSpan)
+        {
+            if (timeSpan < CommonPeriods.First().TimeSpan)
                 return ComputationPeriod.Unknown;
 
-            if (averageDuration > CommonPeriods.Last().TimeSpan)
+            if (timeSpan > CommonPeriods.Last().TimeSpan)
                 return CommonPeriods.Last().Period;
 
-            return CommonPeriods.First(p => averageDuration < p.TimeSpan).Period;
+            return CommonPeriods.First(p => timeSpan <= p.TimeSpan).Period;
         }
 
         private static readonly List<(ComputationPeriod Period, TimeSpan TimeSpan)> CommonPeriods =
             new List<(ComputationPeriod Period, TimeSpan TimeSpan)>
             {
-                (ComputationPeriod.Minutes, TimeSpan.FromMinutes(1)),
-                (ComputationPeriod.Hourly, TimeSpan.FromHours(1)),
+                (ComputationPeriod.Minutes, TimeSpan.FromMinutes(5)),
+                (ComputationPeriod.Hourly, TimeSpan.FromHours(3)),
                 (ComputationPeriod.Daily, TimeSpan.FromDays(1)),
                 (ComputationPeriod.Weekly, TimeSpan.FromDays(7)),
                 (ComputationPeriod.Monthly, TimeSpan.FromDays(28)),
