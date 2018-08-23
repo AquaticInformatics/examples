08ba47a7dcef69a49bf9bf3cf68c15490e8b31bf
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2018-08-23 09:07:09 -0700

    PF-179 Refactoring: Extracted ComputationPeriodEstimator class from the Exporter class

--- SosExporter/Exporter.cs
+++ SosExporter/Exporter.cs
@@ -443,7 +443,7 @@ namespace SosExporter
 
                 if (period == ComputationPeriod.Unknown)
                 {
-                    period = InferPeriodFromRecentPoints(timeSeries);
+                    period = ComputationPeriodEstimator.InferPeriodFromRecentPoints(timeSeries);
                     daysToExtract = Context.Config.MaximumPointDays[period];
                 }
 
@@ -551,66 +551,6 @@ namespace SosExporter
             // Otherwise fall back to the "I don't know" setting
             return ComputationPeriod.Unknown;
         }
-
-        private ComputationPeriod InferPeriodFromRecentPoints(TimeSeriesDataServiceResponse timeSeries)
-        {
-            const int pointCount = 100;
-
-            var recentPoints = timeSeries.Points
-                .Skip(timeSeries.Points.Count - pointCount)
-                .ToList();
-
-            if (recentPoints.Count < 2)
-                return ComputationPeriod.Unknown;
-
-            // Create some bins with zero counts
-            var periodFrequency = new Dictionary<ComputationPeriod,int>();
-
-            for (var i = 0; i < recentPoints.Count - 1; ++i)
-            {
-                var timeSpan = recentPoints[i + 1].Timestamp.DateTimeOffset
-                    .Subtract(recentPoints[i].Timestamp.DateTimeOffset);
-
-                var period = FindClosestPeriod(timeSpan);
-
-                if (!periodFrequency.ContainsKey(period))
-                {
-                    periodFrequency.Add(period, 0);
-                }
-
-                periodFrequency[period] += 1;
-            }
-
-            // Find the most frequent bin
-            var mostCommonPeriod = periodFrequency
-                .Select(kvp => (kvp.Key, kvp.Value))
-                .OrderByDescending(kvp => kvp.Item2)
-                .First().Item1;
-
-            return mostCommonPeriod;
-        }
-
-        private static ComputationPeriod FindClosestPeriod(TimeSpan timeSpan)
-        {
-            if (timeSpan < CommonPeriods.First().TimeSpan)
-                return ComputationPeriod.Unknown;
-
-            if (timeSpan > CommonPeriods.Last().TimeSpan)
-                return CommonPeriods.Last().Period;
-
-            return CommonPeriods.First(p => timeSpan <= p.TimeSpan).Period;
-        }
-
-        private static readonly List<(ComputationPeriod Period, TimeSpan TimeSpan)> CommonPeriods =
-            new List<(ComputationPeriod Period, TimeSpan TimeSpan)>
-            {
-                (ComputationPeriod.Minutes, TimeSpan.FromMinutes(5)),
-                (ComputationPeriod.Hourly, TimeSpan.FromHours(3)),
-                (ComputationPeriod.Daily, TimeSpan.FromDays(1)),
-                (ComputationPeriod.Weekly, TimeSpan.FromDays(7)),
-                (ComputationPeriod.Monthly, TimeSpan.FromDays(28)),
-                (ComputationPeriod.Annual, TimeSpan.FromDays(365)),
-            };
 
         private void FilterTimeSeriesPoints(TimeSeriesDataServiceResponse timeSeries)
         {
