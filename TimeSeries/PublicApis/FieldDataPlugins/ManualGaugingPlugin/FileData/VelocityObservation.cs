9fee0ce00d68c678a96b92afcffe27e12188d132
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2018-10-29 17:45:26 -0700

    Issue-86 Updated SDK
    Also yanked out the stale field data plugin examples

--- TimeSeries/PublicApis/FieldDataPlugins/ManualGaugingPlugin/FileData/VelocityObservation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ManualGaugingPlugin.FileData
-{
-    public class VelocityObservation
-    {
-        public double ObservationDepth { get; set; }
-        public int Revolutions { get; set; }
-        public int IntervalInSeconds { get; set; }
-    }
-}
